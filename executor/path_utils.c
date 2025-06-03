6d5df5ff6d31bcc2865b3aafe34b4183bb00c564
Marjo412 <marjorie_4.28@hotmail.com>
2025-06-03 15:01:39 +0200

    push

--- executor/path_utils.c
+++ executor/path_utils.c
@@ -6,7 +6,7 @@
 /*   By: mrosset <mrosset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 13:50:49 by mrosset           #+#    #+#             */
-/*   Updated: 2025/06/03 14:36:07 by mrosset          ###   ########.fr       */
+/*   Updated: 2025/06/03 14:44:38 by mrosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ char	*get_path()
 /*
 **envp is an array containing  all the environment variables.
 **find_path_variable : this function itarate through envp. If a string
-	begining with "PATH="
+	begining with "PATH=" is found, the function return a ptr to the rest
+	of the string ("bin:/usr/bin:/sbin...").
 */
