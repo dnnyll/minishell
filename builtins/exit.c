6ea3b4f2422ecd81727b0482ca0d2d35c726cc88
Marjo412 <marjorie_4.28@hotmail.com>
2025-06-18 19:54:08 +0200

    export

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: mrosset <mrosset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 14:50:29 by mrosset           #+#    #+#             */
-/*   Updated: 2025/06/18 15:54:03 by mrosset          ###   ########.fr       */
+/*   Updated: 2025/06/18 17:14:07 by mrosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,14 @@ int	exit_builtin(char **args, t_data *data)
 }
 
 /*
-**is_numeric:
-**exit_builtin:
+**is_numeric: Checks if the given string is a valid int, accept an optional
+	sign (+ or -). Checks that the following chars are digits and return 1
+	if it's a number or 0.
+**exit_builtin: This function handles the exit command. prints "exit" on the
+	error output, without args exit exits with code 0. With non numeric args
+	displays an error and exits with code 2. With multiple args displays an
+	error and returns 1 but doesn't exit. With a sigle arg use atoi to convert
+	it and ensure that the code is between 0 and 255 and exit with this code.
+**256: a process return always a number between 0 and 255 (unsigned int sur
+	8 bits).
 */
--- builtins/export.c
+++ builtins/export.c
@@ -6,8 +6,35 @@
 /*   By: mrosset <mrosset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 14:50:34 by mrosset           #+#    #+#             */
-/*   Updated: 2025/06/15 14:50:37 by mrosset          ###   ########.fr       */
+/*   Updated: 2025/06/18 17:50:18 by mrosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	no_args_export(t_data *data)
+{
+	int		i;
+	int		name_len;
+	char	*equal_sign;
+
+	i = 0;
+	while (data->envp[i])
+	{
+		equal_sign = ft_strchr(data->envp[i], '=');
+		if (equal_sign)
+		{
+			name_len = equal_sign - data->envp[i];
+			printf("declare -x %. *s=\"%s\"\n", name_len, data->envp[i],
+				equal_sign + 1);
+		}
+		else
+			printf("declare -x %s\n", data->envp[i]);
+		i++;
+	}
+	return (0);
+}
+
+/*
+**no_args_export:
+*/
