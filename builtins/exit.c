8357720f1eea404919009866eea88d76d4479fd4
Marjo412 <marjorie_4.28@hotmail.com>
2025-06-18 16:05:04 +0200

    env and exit

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,8 +6,55 @@
 /*   By: mrosset <mrosset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 14:50:29 by mrosset           #+#    #+#             */
-/*   Updated: 2025/06/15 14:50:32 by mrosset          ###   ########.fr       */
+/*   Updated: 2025/06/18 15:54:03 by mrosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	is_numeric(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str || !str[0])
+		return (0);
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	exit_builtin(char **args, t_data *data)
+{
+	int	exit_code;
+
+	(void)data;
+	write(2, "exit\n", 5);
+	if (!args[1])
+		exit(0);
+	if (!is_numeric(args[1]))
+	{
+		write(2, "exit: numeric argument required\n", 32);
+		exit(2);
+	}
+	if (args[2])
+	{
+		write(2, "exit: too many arguments\n", 25);
+		return (1);
+	}
+	exit_code = ft_atoi(args[1]) % 256;
+	if (exit_code < 0)
+		exit_code += 256;
+	exit(exit_code);
+}
+
+/*
+**is_numeric:
+**exit_builtin:
+*/
