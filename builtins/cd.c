b7156a497d4c96fe3213a3445da305ea753b9775
Marjo412 <marjorie_4.28@hotmail.com>
2025-06-18 10:25:47 +0200

    continue cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,11 +6,41 @@
 /*   By: mrosset <mrosset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 14:50:08 by mrosset           #+#    #+#             */
-/*   Updated: 2025/06/17 17:16:32 by mrosset          ###   ########.fr       */
+/*   Updated: 2025/06/18 10:08:18 by mrosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char	*get_env_var(char *name, char **envp)
+{
+	int	i;
+	int	len;
+
+	len = ft_strlen(name);
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], name, len) == 0 && envp[i][len] == '=')
+			return (envp[i] + len + 1);
+		i++;
+	}
+	return (NULL);
+}
+
+static char	*handle_cd_dash(char **envp)
+{
+	char	*oldpwd;
+
+	oldpwd = get_env_var("OLDPWD", envp);
+	if (!oldpwd)
+	{
+		write(2, "cd: OLDPWD not set\n", 19);
+		return (NULL);
+	}
+	printf("%s\n", oldpwd);
+	return (oldpwd);
+}
 
 static char	*get_cd_target(char **args, char **envp)
 {
@@ -35,3 +65,10 @@ int	cd_builtin(char **args, char **envp)
 	}
 	retrun (0);
 }
+
+/*
+**get_env_var:
+**handle_cd_dash:
+**get_cd_target:
+**cd_builtin:
+*/
