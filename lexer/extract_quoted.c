7761d7434c768b0f3fa3e149a7b1ff357353382c
dnnyll <leal47danny@gmail.com>
2025-07-10 10:31:06 +0200

    need to handle multiline input

--- lexer/extract_quoted.c
+++ lexer/extract_quoted.c
@@ -39,26 +39,23 @@ t_lexer_result	extract_quoted(const char *input, int i)
 	len = get_quoted_length(input + i + 1, quote);	// Find closing quote
 	if (len == -1)
 	{
+		//HANDLE MULTILINE HERE?! <=================
 		fprintf(stderr, "syntax error: unterminated quote\n");
 		return ((t_lexer_result){NULL, -1});		// Abort lexer
 	}
-
 	token_value = ft_substr(input, i + 1, len);		// Copy string between quotes
 	if (!token_value)
 		return ((t_lexer_result){NULL, i});
 
 	token = create_token(token_value, WORD);		// Create token object
 	free (token_value);								// Free temporary buffer after use
-
 	if (!token)
 		return ((t_lexer_result){NULL, i});
-
 	// Save quote type info for later use (e.g., expansion rules)
 	if (quote == '\'')
 		token->quote = SINGLE_QUOTE;
 	else
 		token->quote = DOUBLE_QUOTE;
-
 	res.token = token;
 	res.index = i + len + 2;					// Move index past both quotes
 	return (res);
